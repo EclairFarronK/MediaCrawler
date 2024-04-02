772e7344314d8d81fdc563243a599695ccb7e06a
PL <1249028596@qq.com>
2024-04-02 16:37:42 +0800

    20240402

---
+++ dev/sync_playwright02.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.byhy.net/_files/stock1.html")
+    print(page.title())
+    page.locator('#kw').fill('通讯\n')
+    page.locator('#go').click()
+    # 打印所有搜索内容
+    lcs = page.locator(".result-item").all()
+    for lc in lcs:
+        print(lc.inner_text())
+    browser.close()
+p.stop()
